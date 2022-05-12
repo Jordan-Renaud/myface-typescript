31dfb7cf830d783963fbbace5d13300f476e3703
Jordan-Renaud <jordan.dj.renaud@gmail.com>
2022-05-12 12:19:12 +0100

    move javascript to correct file

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Add your JavaScript here.
+document.addEventListener("DOMContentLoaded", function () {
+  const changeColourButton = document.getElementById("new-colour-button");
+  changeColourButton.addEventListener("click", changeColour);
+  const posts = document.getElementsByClassName("post");
+  function changeColour() {
+    for (let i = 0; i < posts.length; i++) {
+      posts[i].style.backgroundColor = `rgb(${Math.random() * 255}, ${
+        Math.random() * 255
+      }, ${Math.random() * 255})`;
+    }
+  }
 });
