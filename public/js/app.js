accaa3ec4411d11415f7a74342703a7609426980
ljenchik <pelagenkolena@gmail.com>
2022-05-13 11:58:23 +0100

    Added blured user page

--- public/js/app.js
+++ public/js/app.js
@@ -4,9 +4,11 @@ document.addEventListener("DOMContentLoaded", function () {
   menuButton.addEventListener("click", showMenu);
   function showMenu() {
     const menuBar = document.getElementById("menuBar");
+    const page = document.getElementById("current-page");
     if (menuBar.style.display === "none" || !menuBar.style.display) {
       menuButton.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>'
       menuBar.style.display = "block";
+      page.style.filter='blur(5px)';
     } else {
       menuBar.style.display = "none";
       menuButton.innerHTML  = '<i class="fa fa-bars"></i>'
