1fe5ffbabfa215a8723e56f82b6592222a88e921
ljenchik <pelagenkolena@gmail.com>
2022-05-13 12:28:08 +0100

    All works except posts page

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function () {
   const menuButton = document.getElementById("menu");
   menuButton.addEventListener("click", showMenu);
@@ -6,13 +5,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuBar = document.getElementById("menuBar");
     const page = document.getElementById("current-page");
     if (menuBar.style.display === "none" || !menuBar.style.display) {
-      menuButton.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>'
+      menuButton.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
       menuBar.style.display = "block";
-      page.style.filter='blur(5px)';
+      menuButton.style.backgroundColor = '#151820';
+      page.style.filter = "blur(5px)";
     } else {
       menuBar.style.display = "none";
-      menuButton.innerHTML  = '<i class="fa fa-bars"></i>'
-      page.style.filter='blur(0px)';
+      menuButton.innerHTML = '<i class="fa fa-bars"></i>';
+      menuButton.style.backgroundColor = '#252a38';
+      page.style.filter = "blur(0px)";
     }
   }
 });
