18166aa8fb3f130f5830db3de1da62ca5b1a22b0
jordan-renaud <jordan.dj.renaud@gmail.com>
2022-05-17 16:27:14 +0100

    fix blur not happening on specific user page

--- public/js/app.js
+++ public/js/app.js
@@ -4,15 +4,16 @@ document.addEventListener("DOMContentLoaded", function () {
   function showMenu() {
     const menuBar = document.getElementById("menuBar");
     const page = document.getElementById("current-page");
+
     if (menuBar.style.display === "none" || !menuBar.style.display) {
       menuButton.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
       menuBar.style.display = "block";
-      menuButton.style.backgroundColor = '#151820';
+      menuButton.style.backgroundColor = "#151820";
       page.style.filter = "blur(5px)";
     } else {
       menuBar.style.display = "none";
       menuButton.innerHTML = '<i class="fa fa-bars"></i>';
-      menuButton.style.backgroundColor = '#252a38';
+      menuButton.style.backgroundColor = "#252a38";
       page.style.filter = "blur(0px)";
     }
   }
